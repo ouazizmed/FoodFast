8729fb94c0d92aa3d63c5200803a1e0b32860d87
ouaziz <ouazizmed2@gmail.com>
2020-12-21 02:21:39 +0100

    introductory to login

--- app/src/main/java/com/zizmotrex/foodfast/introdactory/OnBoarding_3Fragment.java
+++ app/src/main/java/com/zizmotrex/foodfast/introdactory/OnBoarding_3Fragment.java
@@ -1,5 +1,6 @@
 package com.zizmotrex.foodfast.introdactory;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zizmotrex.foodfast.R;
+import com.zizmotrex.foodfast.login.LoginActivity;
 
 public class OnBoarding_3Fragment extends Fragment {
 
 
+    FloatingActionButton floatingActionButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,6 +24,14 @@ public class OnBoarding_3Fragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_on_boarding_3, container, false);
 
+        floatingActionButton = root.findViewById(R.id.fab_log);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
     }
 }
